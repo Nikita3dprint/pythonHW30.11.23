5abf596448996d79d4a5e15668456b89044d5be2
Nikita <nikita.sergeevich.cc@gmail.com>
2023-11-29 14:40:57 +0300

    Initial commit

---
+++ 2.py
@@ -0,0 +1,6 @@
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            for w in range(2):
+                if (not(x and y) and (z or not x) or w) == 0:
+                    print(x, y, z, w)
